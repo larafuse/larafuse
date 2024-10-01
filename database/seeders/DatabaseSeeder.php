3c275400826a92c4ba8b9ce0ec3b658b149e454b
Renan Joppert <renanljp@gmail.com>
2024-10-01 11:46:24 -0300

    Changing user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password')
         ]);
     }
 }
